12872fd72fdb6f6260c59c2ef3dde0a6037cdf27
Ben <ford2bm@gmail.com>
2025-06-13 12:35:53 -0600

    Add entrypoint scripts for app and publisher containers

---
+++ app_entrypoint.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/sh
+set -e
+
+echo "Installing Node.js dependencies..."
+npm install
+
+echo "Waiting for ActiveMQ to be fully ready..."
+sleep 10
+
+echo "Starting app.js..."
+node app.js
---
+++ publisher_entrypoint.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/sh
+set -e
+
+echo "Installing Python dependencies..."
+pip install -r requirements.txt
+
+echo "Waiting for ActiveMQ to be fully ready..."
+sleep 10
+
+echo "Starting publisher.py..."
+python -u publisher.py
